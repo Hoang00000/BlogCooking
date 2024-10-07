065aef539d1f4a1f5599cedf3c1f7c24c5b842eb
XxHoangxX <maithanhhoang000@gmail.com>
2024-10-07 11:55:47 +0700

    setup webapp

--- Program.cs
+++ Program.cs
@@ -1,26 +1,25 @@
-var builder = WebApplication.CreateBuilder(new WebApplicationOptions
-            {
-                WebRootPath = "MyWebRoot"
-            });
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
 var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
 app.UseStaticFiles();
-app.MapGet("/", () => "Hello World!");
-app.MapGet("/first", async context =>
-    {
-        string html = @"
-                    <!DOCTYPE html>
-                    <html>
-                    <head>
-                        <meta charset=""UTF-8"">
-                        <title>Trang web đầu tiên</title>
-                    </head>
-                    <body>
-                        <p>Đây là trang web đầu tiên</p>
-                    </body>
-                    </html>
-        "; 
-        
-        await context.Response.WriteAsync(html);
-    });
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
 
 app.Run();
